25ca7171ce6c509b3002635e9dec5482853fc8a7
Arthur E. Jones <PartyLich@gmail.com>
2019-09-04 19:07:31 -0400

    docs: update documentation/comments

--- models/Animation.js
+++ models/Animation.js
@@ -1,5 +1,17 @@
 define(function () {
+  /**
+   * Animation class
+   * @class
+   */
   class Animation {
+    /**
+     * Animation constructor
+     * @param {object} opt
+     * @param {Number} opt.[firstFrame=0] first frame index
+     * @param {Number} opt.[length=0]     number of frames
+     * @param {Number} opt.[repeat=0]
+     * @param {[type]} opt.[fps=60] framerate
+     */
     constructor({
       firstFrame = 0,
       length = 0,
