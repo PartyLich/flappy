3b400a1ae03214d8cb36b580843221118f3b74a7
Arthur E. Jones <PartyLich@gmail.com>
2019-09-03 21:35:48 -0400

    refactor: convert Ball to es6 class

--- models/Ball.js
+++ models/Ball.js
@@ -1,29 +1,22 @@
 define(['Coord'], function(Coord){
+  class Ball extends Coord {
+    constructor({x = 0, y = 0, r = 0} = {}) {
+      super({x: x, y: y});
 
-  //class Ball extends Coord {
-  function Ball(x, y, r) {
-    //Super class constructor
-    Coord.call(this, {x:x, y:y});
+      this.r = r;
+    }
 
-    this.r = (r || 0);
+    /**
+     *  Renders this object on the supplied canvas rendering context
+     *  @param {CanvasRenderingContext2D} ctx
+     */
+    draw(ctx) {
+      ctx.beginPath();
+      ctx.moveTo(this.x, this.y);
+      ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2, true);
+      ctx.fill();
+    }
   }
-
-  //Inherit Coord
-  Ball.prototype = new Coord();
-
-  //Correct the constructor pointer.
-  Ball.prototype.constructor = Ball;
-
-  /**
-   *  Renders this object on the supplied canvas rendering context
-   *  @param {CanvasRenderingContext2D} ctx
-   */
-  Ball.prototype.draw = function (ctx) {
-    ctx.beginPath();
-    ctx.moveTo(this.x, this.y);
-    ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2, true);
-    ctx.fill();
-  };
 
   return Ball;
 });
