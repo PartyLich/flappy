b9484bae914249c937b0a7c9e9a7df6c78b67e19
Arthur E. Jones <PartyLich@gmail.com>
2019-09-04 19:06:39 -0400

    refactor: rewrite Animation constructor
    Use object destructuring and default values. Eliminates need for
    separate function.

--- models/Animation.js
+++ models/Animation.js
@@ -1,34 +1,15 @@
 define(function () {
   class Animation {
-    constructor(opts) {
-      this.firstFrame = 0;  //
-      this.length = 0;     //
-      this.repeat = 0;
-      this.fps = 0;
-
-      this.init(opts);
-    }
-
-    init(opt) {
-      var key = null,
-        default_args = {
-          firstFrame : 0,
-          length : 0,
-          repeat : 0,
-          fps : 60
-        };
-
-      opt = (opt || default_args);
-
-      for(key in default_args) {
-        if(typeof opt[key] == "undefined") opt[key] = default_args[key];
-      }
-
-      // opt[] has all the data - user provided and optional.
-      for(key in opt) {
-        //console.log(key + " = " + opt[key]);
-        this[key] = opt[key];
-      }
+    constructor({
+      firstFrame = 0,
+      length = 0,
+      repeat = 0,
+      fps = 60,
+    } = {}) {
+      this.firstFrame = firstFrame;
+      this.length = length;
+      this.repeat = repeat;
+      this.fps = fps;
     }
   }
 
