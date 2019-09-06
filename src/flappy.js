8e5b28fe4130d2a5e275c1186d49b732a82a2d83
Arthur E. Jones <PartyLich@gmail.com>
2019-09-06 15:35:16 -0400

    refactor: transition from requirejs to webpack

--- src/flappy.js
+++ src/flappy.js
@@ -1,35 +1,9 @@
 /**
  *
  */
-require.config({
-  paths: {  // Configure library/module paths.
-    // 'jquery' : 'https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min',
-    'jquery' : 'lib/jquery-1.8.2.min',
-    'haml': 'lib/haml',
+import $ from 'jquery';
+import Engine from './engine';
 
-    'mathLib': 'lib/mathLib',
-    'Mediator': 'lib/mediator',
-    'Graphics': 'lib/Graphics',
-    'Physics': 'lib/physics',
-
-    'animation': 'models/Animation',
-    'ball': 'models/Ball',
-    'Coord': 'models/Coord',
-
-    'player': 'models/Player',
-    'pipe': 'models/Pipe',
-    'StopWatch': 'models/StopWatch',
-  },
-  // Shim config FTW: http://requirejs.org/docs/api.html#config-shim
-  shim: {
-    'haml': {
-      exports: 'Haml',
-    },
-  },
-});
-
-
-require(['jquery', 'haml', 'engine'], function ($, Haml, Engine) {
   $(function () {
     // RequestAnimationFrame shim
     const requestAnimationFrame = window.requestAnimationFrame
@@ -41,4 +15,3 @@ require(['jquery', 'haml', 'engine'], function ($, Haml, Engine) {
 
     engine.home();
   });
-});
